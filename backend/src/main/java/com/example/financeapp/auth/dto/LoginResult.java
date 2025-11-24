aa3a62b449cd7cc3755a145fb30017c584f1cb84
Tùng <nguyentung16092005@gmail.com>
2025-11-25 02:51:24 +0700

    fix

--- backend/src/main/java/com/example/financeapp/auth/dto/LoginResult.java
+++ backend/src/main/java/com/example/financeapp/auth/dto/LoginResult.java
@@ -2,8 +2,10 @@ package com.example.financeapp.auth.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginResult {
     private Long userId;
