2d9f847734bd8d1cfc6081f1c52ad5384cadefa8
tuanburh1134 <tuan75035@gmail.com>
2025-12-05 03:49:13 +0700

    feat:fix

--- backend/src/main/java/com/example/financeapp/service/ExchangeRateService.java
+++ backend/src/main/java/com/example/financeapp/service/ExchangeRateService.java
@@ -16,7 +16,7 @@ public interface ExchangeRateService {
      * big C bú source 1ssss
      * g 11 arial bolth bảon
      * gggg:)))ssssssádasd
-     * @param fromCurrency Loại tiền nguồn (VD: "USD")
+     * @param fromCurrency Loại tiền nguồn (VD: "USD")ádasd
      * @param toCurrency Loại tiền đích (VD: "VND")
      *                   mảiana
      * @return Tỷ giá (VD: 1 USD = 24,350 VND → return 24350.0)
