71bccf68127ea9a2e1a6fde8d0b06041a46846de
tuanburh1134 <tuan75035@gmail.com>
2025-12-05 03:45:25 +0700

    feat:fix

--- backend/src/main/java/com/example/financeapp/service/ExchangeRateService.java
+++ backend/src/main/java/com/example/financeapp/service/ExchangeRateService.java
@@ -12,6 +12,7 @@ public interface ExchangeRateService {
      * lấy tỉ giá chuyển nhượng cổ phần
      * git checkout bfeature
      * intelliji fact
+     * fix main develop origin
      * @param fromCurrency Loại tiền nguồn (VD: "USD")
      * @param toCurrency Loại tiền đích (VD: "VND")
      * @return Tỷ giá (VD: 1 USD = 24,350 VND → return 24350.0)
