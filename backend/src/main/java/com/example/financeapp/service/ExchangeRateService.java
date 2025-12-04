d121611cdbd5627cdaa060d085ef0400fdaab632
tuanburh1134 <tuan75035@gmail.com>
2025-12-05 03:46:31 +0700

    feat:fix

--- backend/src/main/java/com/example/financeapp/service/ExchangeRateService.java
+++ backend/src/main/java/com/example/financeapp/service/ExchangeRateService.java
@@ -16,7 +16,7 @@ public interface ExchangeRateService {
      * @param fromCurrency Loại tiền nguồn (VD: "USD")
      * @param toCurrency Loại tiền đích (VD: "VND")
      * @return Tỷ giá (VD: 1 USD = 24,350 VND → return 24350.0)
-     * return facrt,in finiti war
+     * return facrt,in finiti war :))))
     BigDecimal getExchangeRate(String fromCurrency, String toCurrency);
 
     /**
